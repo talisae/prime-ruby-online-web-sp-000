2a0fa4577546a0d034482833f0f4e65914d263c1
Talisa Erazo <talisaerazo@gmail.com>
2018-11-27 18:55:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -7,14 +7,3 @@ def prime?(integer)
     end
   end
 end
-
-
-def prime?(num)
-  if num < 0 or num == 0 or num == 1
-    return false
-  else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
-    end
-  end
-end
